1f02a53ffa9cb228da3fb04f49840bef6f037dbf
Kileana27 <blond.camillelm@gmail.com>
2024-05-18 16:11:20 +0200

    Add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { DirectivesComponent } from './directives/directives.component';
@@ -8,12 +8,12 @@ import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BlockComponent, UserProfileComponent, DirectivesComponent, NgClass],
+  imports: [RouterLink, RouterOutlet, BlockComponent, UserProfileComponent, DirectivesComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
 export class AppComponent {
-
+  title: string = "AngularBlocks"
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { BlockComponent } from './block/block.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'home',
+    component: HomeComponent
+},
+{
+    path: 'contact',
+    component: ContactComponent
+},
+{
+    path: 'user',
+    component: UserProfileComponent
+},
+{
+    path: 'directives',
+    component: DirectivesComponent
+},
+{
+    path: 'blocks',
+    component: BlockComponent
+}
+];
+
